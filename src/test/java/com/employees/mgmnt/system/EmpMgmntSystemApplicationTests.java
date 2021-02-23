331bba91bcdf671f3e7175e54ccfa3431c8c5075
subhroy <subhasis.it@gmail.com>
2021-02-24 01:37:31 +0530

    Added Test class

--- src/test/java/com/employees/mgmnt/system/EmpMgmntSystemApplicationTests.java
+++ src/test/java/com/employees/mgmnt/system/EmpMgmntSystemApplicationTests.java
@@ -1,13 +1,25 @@
 package com.employees.mgmnt.system;
 
+import com.employees.mgmnt.system.service.EmployeeManagementService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmpMgmntSystemApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
 
+	@Autowired
+	EmployeeManagementService employeeManagementService;
+
+	@Test
+	public void testGetAllEmployee(){
+		System.out.println("\n\n Result All Employees :: \n\n ");
+		employeeManagementService.getAllEmployees().stream().forEach(employeeDTO -> {
+			System.out.println(employeeDTO.toString());
+		});
+	}
 }
